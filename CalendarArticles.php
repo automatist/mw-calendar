5e499dfb585982bcbbd1b2786128d1f3fa2cce56
Michael Murtaugh <mm@automatist.org>
2019-10-22 12:50:15 +0200

    replaced article->getContent

--- CalendarArticles.php
+++ CalendarArticles.php
@@ -69,7 +69,7 @@ function newFromRedirect($text) {
 		if( $article->isRedirect() && $this->setting('disableredirects') ) return '';
 		
 		 while($article->isRedirect() && $redirectCount < 10){
-			 $redirectedArticleTitle = newFromRedirect($article->getContent());
+			 $redirectedArticleTitle = newFromRedirect($article->getPage()->getContent());
 			 $article = new Article($redirectedArticleTitle);
 			 $redirectCount += 1;
 		 }
