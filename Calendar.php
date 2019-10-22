4539efa0ff4b8708c478b022405c4e787c6a27e0
Michael Murtaugh <mm@automatist.org>
2019-10-22 13:15:15 +0200

    updated version to 3.8.5 --- can I do that?

--- Calendar.php
+++ Calendar.php
@@ -86,7 +86,7 @@ if (!defined('MEDIAWIKI')) {
 	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
 }
 
-$gCalendarVersion = "v3.8.4 (8/18/2009)";
+$gCalendarVersion = "v3.8.5 (22/10/2019)";
 //$gCalendarVersion = "trunk/beta";
 
 # Credits	
